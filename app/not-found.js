77173249820c9d0ed60bc3e5406e1f1ca5b318b4
PhiliaConciergerie <occi.servicespro@gmail.com>
2026-07-13 00:24:48 +0200

    Create not-found.js

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Logement introuvable</h1>
+      <p>Vérifiez le lien qui vous a été envoyé, ou contactez Philia Conciergerie.</p>
+    </main>
+  );
+}
