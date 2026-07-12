ae5ddeb40f0a995c124d23979123c02d423df5a0
PhiliaConciergerie <occi.servicespro@gmail.com>
2026-07-13 00:41:58 +0200

    Add logements data for Le Saint-Pierre apartment

---
+++ data/logements.js
@@ -0,0 +1,16 @@
+export const logements = {
+  'saint-pierre': {
+    nom: 'Le Saint-Pierre',
+    adresse: '12 rue des Lilas, 31000 Toulouse',
+    description:
+      "Un appartement lumineux au cœur de Toulouse, à deux pas de la place Saint-Pierre et de ses terrasses.",
+    arrivee: 'à partir de 16h00',
+    depart: 'avant 11h00',
+    photos: [],
+    recommandations: [
+      { nom: 'Boulangerie du coin', description: 'Croissants et pain frais, à 3 minutes à pied.' },
+      { nom: 'Place Saint-Pierre', description: 'Bars et restaurants au bord de la Garonne.' },
+    ],
+    contactUrgence: '06 00 00 00 00',
+  },
+};
